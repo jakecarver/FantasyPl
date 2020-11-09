e8898375fa07803d120e84a7e64db8a3082dbe73
jakecarver <54565070+jakecarver@users.noreply.github.com>
2020-11-09 01:30:46 -0800

    Documentation

--- src/scraper.py
+++ src/scraper.py
@@ -9,28 +9,32 @@ import sys
 import time
 
 def fixScraper (EMAIL, PASSWORD):
-    
 
-    #path = 'C:/Users/jakep/Downloads/chromedriver_win32/chromedriver.exe'
+    #Install webdrivers if necessary and create driver instance
     driver = webdriver.Chrome(ChromeDriverManager().install())
+
+    #Open login page
     driver.get('https://www.fantasyfootballfix.com/price/')
-    print(driver.page_source)
+
+    #Enter user email and ID to access site
     email = driver.find_element_by_id('id_email').send_keys(EMAIL)
     password = driver.find_element_by_id('id_password1').send_keys(PASSWORD)
     login = driver.find_element_by_id('loadSquad').click()
     
+    #Navigate to page with the dataset
     driver.get('https://www.fantasyfootballfix.com/price/')
 
+    #Wait for data table to load
     element = WebDriverWait(driver, 50).until(
                     EC.presence_of_element_located((By.ID, "playerPriceAll"))
                 )
+    #Generate BeautifulSoup and find the data table
     soup = BeautifulSoup(driver.page_source, 'lxml')
-    #table = soup.find_all( id ='playerPriceAll')[0]
     table = soup.find( id ='playerPriceAll')
     
+    #Index columns for later use
     cols = table.find_all("th")
-    df = pd.DataFrame([], columns=['Name','Position','Team','CV',
-        'week1','week2','week3','week4','week5','week6'])
+    #Dictionary holding column indices
     colDict = {}
     for i in range (0,len(cols)):
         if cols[i].getText() == 'Name':
@@ -42,125 +46,158 @@ def fixScraper (EMAIL, PASSWORD):
         elif cols[i].getText() == 'CV':
             colDict['CV'] = i
     
+    #Initialize dictionary for popup column indexes
     innerColDict = {}
-    rows = table.find_all("tr")
+    
+    #End driver instance
     driver.quit()
 
+    #Initialize dataframe
+    df = pd.DataFrame([], columns=['Name','Position','Team','CV',
+        'week1','week2','week3','week4','week5','week6'])
+    
+    #Initialize page count
     page = 0
+
+    #For every page in the table
     while True:
+        #print page number
         print('PAGE: '+str(page))
+
+        #Create new driver  instance and login (same as before)
+        driver = webdriver.Chrome(ChromeDriverManager().install())
         driver.get('https://www.fantasyfootballfix.com/price/')
         email = driver.find_element_by_id('id_email').send_keys(EMAIL)
         password = driver.find_element_by_id('id_password1').send_keys(PASSWORD)
         login = driver.find_element_by_id('loadSquad').click()
-        
         driver.get('https://www.fantasyfootballfix.com/price/')
+
+        #Wait for table to generate
         element = WebDriverWait(driver, 50).until(
                     EC.presence_of_element_located((By.ID, "playerPriceAll"))
                 )
         
+        #Attempt to navigate to correct page of the table, clicking for each page number
         try:
             for i in range (0, page):
                 WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"playerPriceAll_paginate\"]/ul/li[7]/a")))
                 WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"playerPriceAll_paginate\"]/ul/li[7]/a"))).click()
-                
+        
+        #On failure, exit process. Once there are no more pages of data to visit, the process will enter here and output to csv
         except: 
             print ('Click Failure')
             break
+
+        #Wait till rows have generated
         element = WebDriverWait(driver, 50).until(
                     EC.presence_of_element_located((By.CSS_SELECTOR, "#playerPriceAll tr"))
                 )
-        while (driver.find_elements_by_xpath("//playerPriceAll[contains(text(),'Loading')]")):
+        #Wait until the "loading" row has disappeared
+        while (driver.find_elements_by_class_name("dataTables_empty")):
                 driver.implicitly_wait(1)
-        #Wait a little longer just in case only half of the table loaded
+
+        #Wait a little longer just in case only half of the table loaded (no good way to verify if table is full)
         driver.implicitly_wait(3)
+
+        #Generate new soup, table element, and list of rows
         soup = BeautifulSoup(driver.page_source, 'lxml')
         table = soup.find( id ='playerPriceAll')
         rows = table.find_all("tr")
         
-        
+        #For every row
         for i in range (1, len(rows)):
 
+            #Data to be output into dataframe
             data = {}
             
+            #Create list of data points and use the index dictionary to input data into our output
             cells = rows[i].find_all("td")
             data['Name']=cells[colDict['Name']].getText().replace(' ','')
-            
             data['Position']=cells[colDict['Position']].getText()
             data['Team']=cells[colDict['Team']].getText()
             data['CV']=float(cells[colDict['CV']].getText())
 
+            #Revisit the price page (closing the popup table)
             driver.get('https://www.fantasyfootballfix.com/price/')
             
+            #try to click to correct table page
             try:
                 for q in range (0, page):
-                    
                     WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"playerPriceAll_paginate\"]/ul/li[7]/a")))
-                    
                     driver.find_element_by_css_selector("#playerPriceAll_paginate > ul > li.next > a").click()
                     
-                    
+            #Return an error and output the rest of our data into the dataframe if error      
             except: 
                 print ('Fail 1')
                 df.append(data, ignore_index=True)
                 break    
             
-
+            #Wait for the new page table to load
             element = WebDriverWait(driver, 50).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, "#playerPriceAll tr"))
                 )
             
+            #Wait for "loading" row to disappear
             while (driver.find_elements_by_xpath("//playerPriceAll[contains(text(),'Loading')]")):
                 driver.implicitly_wait(1)
+
+            #Open popup table
             driverRows = driver.find_elements_by_css_selector('#playerPriceAll tr')
-            
-            
-            
             moreInfo = driverRows[i].find_element_by_tag_name('a').click()
-            
-            
-            
+
+            #Scraping the popup table
             try:
+                #Wait until the appropriate tab appears
                 element = WebDriverWait(driver, 50).until(
                     EC.presence_of_element_located((By.ID, "fixturetab"))
                 )
+
+                #Click on tab
                 driver.find_element_by_id('fixturetab').click()
+
+                #Wait for table to appear
                 element = WebDriverWait(driver, 50).until(
                     EC.presence_of_element_located((By.ID, "fixturetable"))
                 )
+
+                #Create a new soup from table
                 newSoup = BeautifulSoup(driver.page_source, 'lxml')
                 innerTable = newSoup.find( id ='fixturetable')
                 innerRows = innerTable.find_all("tr")
                 
-                driverInnerTable = driver.find_element_by_id('fixturetable')
-                driverInnerRows = driverInnerTable.find_elements_by_tag_name('tr')
-                if innerColDict == {}:
+                #Find element in driver
+                #driverInnerTable = driver.find_element_by_id('fixturetable')
+                #driverInnerRows = driverInnerTable.find_elements_by_tag_name('tr')
 
-                    
+                #If inner table dictionary is empty, fill with header indexes
+                if innerColDict == {}:
+                    #Find all headers
                     innerCols = innerTable.find_all("th")
-                    
+                    #search for the correct strings and add to dictionary
                     for j in range(0,len(innerCols)):
-                        
                         if 'points' in innerCols[j].getText():
-                            
                             innerColDict['Points'] = j
                             break
-                    
+                
+                #Add prediction data for every week
                 for k in range (1, len(innerRows)):
-                    
                     innerCells = innerRows[k].find_all("td")
-                    
                     data['week'+str(k)]=float(innerCells[innerColDict['Points']].getText().replace(' ',''))
                 
                 print (data)
+
+                #Add data to dataframe
                 df.append(data, ignore_index=True)
-                
+            
+            #Return error if popup scraping fails
             except:
                 print ("ERROR")
                 driver.quit()
+        #Once all rows on table page are completed, iterate page and close current driver
         page += 1
         driver.quit()
-            
+
+    #Create csv and end    
     df.to_csv('out.csv', index=False)  
     driver.quit()
 
